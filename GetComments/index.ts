7ff9e701f8bad998f19386f87ed13b723896134a
sspeaks610 <marialith191@gmail.com>
2021-04-17 14:36:51 -0700

    added an azure function to determine if a streamer is live

--- GetComments/index.ts
+++ GetComments/index.ts
@@ -3,7 +3,6 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     const rows: Row[] = context.bindings.inputTable ?? [];
 
-    context.log("AAAAAAAAAAAAAAAAAAAAAAAAAA");
     rows.sort((a, b) => a.RowKey > b.RowKey ? 1 : -1)
     const messages: Message[] = rows.map(({ message, date, name }) => ({ message, name, date }));
 
---
+++ GetIsStreamerActive/index.ts
@@ -0,0 +1,43 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+
+import axios, { AxiosRequestConfig } from 'axios';
+
+const client_id = process.env["twitch_client_id"];
+const client_secret = process.env["twitch_client_secret"];
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    const isLive = await getIsStreamerOnline(req.query.streamer);
+
+    context.res = { body: { isLive } };
+    context.done();
+};
+
+
+const getIsStreamerOnline = async (streamer: string) => {
+    const auth_token: string = await getAuthToken();
+
+    return await axios.get(`https://api.twitch.tv/helix/search/channels?query=${streamer}`, {
+        headers: {
+            'Authorization': `Bearer ${auth_token}`,
+            'Client-Id': client_id
+        }
+    })
+        .then(response => response.data.data[0].is_live);
+};
+
+const getAuthToken = async () => await axios.post('https://id.twitch.tv/oauth2/token' +
+    `?client_id=${client_id}` +
+    `&client_secret=${client_secret}` +
+    `&grant_type=client_credentials`)
+    .then(resp => resp.data)
+    .then((data: TwitchAuthResponse) => data.access_token);
+
+interface TwitchAuthResponse {
+    access_token: string;
+    refresh_token: string;
+    expires_in: number;
+    scope: string[];
+    token_type: "bearer";
+}
+
+export default httpTrigger;
