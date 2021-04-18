7aafb20405b11765b6b8d3025875e813b9c4d043
sspeaks610 <marialith191@gmail.com>
2021-04-17 21:46:43 -0700

    promise.all

--- static/toggleStreamerPanes.js
+++ static/toggleStreamerPanes.js
@@ -4,29 +4,31 @@
     const panes = document.querySelectorAll('.streamer-pane');
     const watchHim = document.querySelector('#watch-him');
 
-    for (let streamer of favStreamers) {
+    const results = await Promise.all(favStreamers
+        .map(async (streamer) => {
+            const isLive = await axios.get(`http://localhost:7071/api/GetIsStreamerActive/${streamer}`).then(response => response.data.isLive);
+            return { streamer, isLive };
+        }));
 
-        const isLive = await axios.get(`/isActive/${streamer}`).then(response => response.data.isLive);
+    const streamer = results.find(result => result.isLive)?.streamer;
 
-        if (isLive) {
-            watchHim.innerHTML = watchHim.innerHTML.replace("Mainteam7", streamer);
-            
-            new Twitch.Embed("twitch-embed", {
-                channel: streamer,
-                layout: 'video',
-                width: '90%',
-                height: '90%',
-                // only needed if your site is also embedded on embed.example.com and othersite.example.com 
-                parent: ["mycatsonfire.com", "localhost"]
-            });
-            
-            panes.forEach(pane => {
-                pane.style.opacity = 1;
-            });
+    if (typeof (streamer) !== undefined) {
+        watchHim.innerHTML = watchHim.innerHTML.replace("Mainteam7", streamer);
 
-           
-            return;
-        }
+        new Twitch.Embed("twitch-embed", {
+            channel: streamer,
+            layout: 'video',
+            width: '90%',
+            height: '90%',
+            // only needed if your site is also embedded on embed.example.com and othersite.example.com 
+            parent: ["mycatsonfire.com", "localhost"]
+        });
+
+        panes.forEach(pane => {
+            pane.style.opacity = 1;
+        });
+
+        return;
     }
     panes.forEach(pane => {
         pane.style.display = "none";
