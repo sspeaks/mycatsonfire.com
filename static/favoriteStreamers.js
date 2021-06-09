f3e92329ed661c0f1c54c2fecb99fa400d9f85ef
Daisy's Dad <sspeaks610@gmail.com>
2021-06-08 18:03:08 -0700

    Trying some client cert auth stuff for fun

--- static/favoriteStreamers.js
+++ static/favoriteStreamers.js
@@ -1,8 +1,22 @@
 const favStreamers = [
     "bloodfox610",
-"mainteam7",
-"One_Promise",
-"Brodock085",
-"day9tv",
-"xaryu"
+    "mainteam7",
+    "One_Promise",
+    "Brodock085",
+    "day9tv",
+    "xaryu"
 ];
+
+// Temporary for now, just to test
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
+const sub = getCookie('subject');
+
+if (sub) {
+    const name = sub.match(/CN=(.*?),/)[1]
+    document.querySelector('#subjectName').innerHTML = name;
+}
